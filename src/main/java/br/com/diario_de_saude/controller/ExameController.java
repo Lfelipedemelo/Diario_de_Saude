9a2abaf901c5a3ba67af8c617603e71cc44e4694
lfelipedemelo <lfelipedemelo@hotmail.com>
2021-09-08 20:32:52 -0300

    Armazenamento de imagem feito

--- src/main/java/br/com/diario_de_saude/controller/ExameController.java
+++ src/main/java/br/com/diario_de_saude/controller/ExameController.java
@@ -1,19 +1,24 @@
 package br.com.diario_de_saude.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.diario_de_saude.repository.ExameRepository;
+import br.com.diario_de_saude.utils.FileUploadUtil;
 import br.com.diario_de_saude.vo.Exame;
+import br.com.diario_de_saude.vo.Usuario;
 
 @Controller
 @RequestMapping("/exames")
@@ -27,14 +32,19 @@ public class ExameController {
 		ModelAndView mv = new ModelAndView("exame");
 		List<Exame> exames = repository.findAll();
 		mv.addObject("exames",exames);
-		
+		mv.addObject("maxDate" ,LocalDate.now());
 		return mv;
 	}
 	
 	@PostMapping
-	public ModelAndView adicionar(@ModelAttribute Exame exame, HttpSession session) {
-		ModelAndView mv = new ModelAndView("redirect:/exame");
-		repository.save(exame);
+	public ModelAndView adicionar(Exame exame, HttpSession session, @RequestParam("imagem") MultipartFile mpFile) throws Exception {
+		ModelAndView mv = new ModelAndView("redirect:/exames");
+		String fileName = StringUtils.cleanPath(mpFile.getOriginalFilename());
+		exame.setUsuario((Usuario) session.getAttribute("usuarioLogado"));
+		exame.setArquivo(fileName);
+		Exame exameSalvo = repository.save(exame);
+		String diretorio = "exame-img/" + exameSalvo.getUsuario().getId();
+		FileUploadUtil.saveFile(diretorio, fileName, mpFile);
 		return mv;
 	}
 }
--- src/main/java/br/com/diario_de_saude/vo/Exame.java
+++ src/main/java/br/com/diario_de_saude/vo/Exame.java
@@ -1,13 +1,15 @@
 package br.com.diario_de_saude.vo;
 
-import java.io.File;
-import java.sql.Blob;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Exame {
@@ -15,11 +17,15 @@ public class Exame {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	private String nome;
-	
-	private File arquivo;
-	
+
+	private String arquivo;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date dtaExame;
+
+	private String descricao;
+
 	@ManyToOne
 	private Usuario usuario;
 
@@ -46,12 +52,37 @@ public class Exame {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	public File getArquivo() {
+
+	public String getArquivo() {
 		return arquivo;
 	}
-	
-	public void setArquivo(File arquivo) {
+
+	public void setArquivo(String arquivo) {
 		this.arquivo = arquivo;
+	}
+
+	public Date getDtaExame() {
+		return dtaExame;
+	}
+
+	public void setDtaExame(Date dtaExame) {
+		this.dtaExame = dtaExame;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCaminhoImagem() {
+		if (arquivo == null) {
+			return null;
+		} else {
+			return "/exame-img/" + usuario.getId() + "/" + arquivo;
+		}
 	}
 
 	@Override
